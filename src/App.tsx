ac43f8c861902f35eb951a0d68aa236ff3d72fdb
Henrique Black <67487181+hnrqblck@users.noreply.github.com>
2021-06-24 17:09:55 -0300

    Aula 3

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,23 @@
-import { Button } from './components/Button';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import { Home } from "./pages/Home";
+import { NewRoom } from "./pages/NewRoom";
+import { Room } from './pages/Room';
+
+import { AuthContextProvider } from './contexts/AuthContext'
 
 function App() {
+
   return (
-    <div>
-      <h1>Hello World</h1>
-      <Button />
-      <Button />
-      <Button />
-    </div>
+    <BrowserRouter>
+      <AuthContextProvider>
+        <Switch> {/* se uma rota for satisfeita o React ira parar de procurar por outra que também satisfaça*/}
+          <Route path="/" exact component={Home} />
+          <Route path="/rooms/new" component={NewRoom} />
+          <Route path="/rooms/:id" component={Room} />
+        </Switch>
+      </AuthContextProvider>
+    </BrowserRouter>
   );
 }
 
